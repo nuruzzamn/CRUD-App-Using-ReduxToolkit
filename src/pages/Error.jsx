cda6c8380cc2874a77f48f79222f11a525f25078
unknown <nur.mnzaman@gmail.com>
2023-09-03 14:55:43 +0600

    [feat] design error page

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,7 +1,13 @@
-import React from "react";
-
 const Error = () => {
-  return <div>Error</div>;
+  return (
+    <div className="error-container">
+      <div className="error-content">
+        <h1 className="error-title"> 404 </h1>
+        <p className="error-description"> Page Not Found </p>
+        <a href="/" className="error-link">Go to Home</a>
+      </div>
+    </div>
+  );
 };
 
 export default Error;
